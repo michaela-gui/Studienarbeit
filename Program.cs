14b17154643117024773790511039d591c5c042a
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2021-04-24 19:28:50 +0200

    Delete Unnötiges.

--- Program.cs
+++ Program.cs
@@ -15,19 +15,6 @@ namespace Studienarbeit
         [STAThread]
         static void Main()
         {
-            Calculation myCalc = new Calculation();
-            int Beleuchtung_Nutzung_Allgemein_Raum_Lux = 300;
-            int Beleuchtung_Nutzung_Allgemein_Arbeitsbereich_Lux = 550;
-            int Beleuchtung_Nutzung_Küche_Arbeitsbereich_Lux = 500;
-            double Reflexion_Allgemein_Schreibtisch = 0.8;
-            double Relexion_Allgemein_Raum = 0.6;
-            double Reflexion_Küche_Küchenplatte = 0.5; // ?
-
-            myCalc.LeuchtdichteBerechnen(Relexion_Allgemein_Raum, Beleuchtung_Nutzung_Allgemein_Raum_Lux);
-            myCalc.LeuchtdichteBerechnen(Reflexion_Küche_Küchenplatte, Beleuchtung_Nutzung_Küche_Arbeitsbereich_Lux);
-
-            Storage myStorage = new Storage();
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
